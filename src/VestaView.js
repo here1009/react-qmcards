c89e8dc816eb5601f58bdebd02ecf0efcf18c644
here1009 <gao_yantao@yeah.net>
2020-02-26 15:57:35 +0800

    use webglrenderer

--- src/VestaView.js
+++ src/VestaView.js
@@ -2,184 +2,97 @@ import React, { Component } from 'react';
 import './VestaView.css'
 import * as THREE from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
-//import { PDBLoader } from 'three/examples/jsm/loaders/PDBLoader';
 import { ATOMCONFIGLoader } from './AtomconfigLoader';
-import { CSS3DRenderer, CSS3DObject, CSS3DSprite } from 'three/examples/jsm/renderers/CSS3DRenderer';
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer';
 import NB from './c2.config';
-import ATOM from './atom7.config';
-import ball from './ball.png';
+import ATOM from './atom5.config';
 
-var camera, scene, renderer;
+var camera, scene, renderer,labelRenderer;
 var controls;
 var root;
 var gmount;
 
-
-var objects = [];
-var tmpVec1 = new THREE.Vector3();
-var tmpVec2 = new THREE.Vector3();
-var tmpVec3 = new THREE.Vector3();
-var tmpVec4 = new THREE.Vector3();
 var offset = new THREE.Vector3();
 
 var visualizationType = 2;
 var loader = new ATOMCONFIGLoader();
+
 var colorSpriteMap = {};
-var baseSprite = document.createElement('img');
 
 function init() {
 
-    //camera = new THREE.PerspectiveCamera(70, gmount.clientWidth / gmount.clientHeight, 1, 5000);
+    camera = new THREE.PerspectiveCamera(70, gmount.clientWidth / gmount.clientHeight, 1, 5000);
     var size=gmount.clientWidth/2;
-    camera = new THREE.OrthographicCamera(-size, size,-size, size, -1, 5000);
+    //camera = new THREE.OrthographicCamera(-size, size,-size, size, -1, 5000);
     camera.zoom=1;
     camera.position.z = 1000;
 
     scene = new THREE.Scene();
 
-    root = new THREE.Object3D();
+    var light = new THREE.DirectionalLight(0xffffff, 0.8);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+
+    var light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(-1, -1, -1);
+    scene.add(light);
+
+    var light = new THREE.AmbientLight(0xffffff, 0.3);
+    scene.add(light);
+
+    root = new THREE.Group();
     scene.add(root);
     //
-
-    renderer = new CSS3DRenderer();
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(gmount.clientWidth, gmount.clientHeight);
     gmount.appendChild(renderer.domElement);
-
     //
-
+    labelRenderer = new CSS2DRenderer();
+    labelRenderer.setSize(gmount.clientWidth, gmount.clientHeight);
+    labelRenderer.domElement.style.position = 'absolute';
+    labelRenderer.domElement.style.top = '0';
+    labelRenderer.domElement.style.pointerEvents = 'none';
+    gmount.appendChild(labelRenderer.domElement);
+    //
     controls = new TrackballControls(camera, renderer.domElement);
-    controls.rotateSpeed = 5.0;
+    controls.minDistance = 500;
+    controls.maxDistance = 2000;
 
     //
-
-    baseSprite.onload = function () {
-
-        //loadMolecule(Caf);
-        loadMolecule(ATOM);
-        //loadMolecule(NB);
-
-    };
-
-    baseSprite.src = ball;
-
+    loadMolecule(ATOM);
+    
     window.addEventListener('resize', onWindowResize, false);
     //
 }
 function showAtoms() {
 
-    for (var i = 0; i < objects.length; i++) {
-
-        var object = objects[i];
-
-        if (object instanceof CSS3DSprite) {
-
-            object.element.style.display = "";
-            object.visible = true;
-
-        } else {
-
-            object.element.style.display = "none";
-            object.visible = false;
-
-        }
-
-    }
-
 }
 function showBonds() {
 
-    for (var i = 0; i < objects.length; i++) {
-
-        var object = objects[i];
-
-        if (object instanceof CSS3DSprite) {
-
-            object.element.style.display = "none";
-            object.visible = false;
-
-        } else {
-
-            object.element.style.display = "";
-            object.element.style.height = object.userData.bondLengthFull;
-            object.visible = true;
-
-        }
-
-    }
-
 }
-
-
 
 function showAtomsBonds() {
 
-    for (var i = 0; i < objects.length; i++) {
-
-        var object = objects[i];
-
-        object.element.style.display = "";
-        object.visible = true;
-
-        if (!(object instanceof CSS3DSprite)) {
-
-            object.element.style.height = object.userData.bondLengthShort;
-
-        }
-
-    }
-
-}
-function colorify(ctx, width, height, color) {
-
-    var r = color.r, g = color.g, b = color.b;
-
-    var imageData = ctx.getImageData(0, 0, width, height);
-    var data = imageData.data;
-
-    for (var i = 0, l = data.length; i < l; i += 4) {
-
-        data[i + 0] *= r;
-        data[i + 1] *= g;
-        data[i + 2] *= b;
-
-    }
-
-    ctx.putImageData(imageData, 0, 0);
-
 }
 
-function imageToCanvas(image) {
-
-    var width = image.width;
-    var height = image.height;
-
-    var canvas = document.createElement('canvas');
-
-    canvas.width = width;
-    canvas.height = height;
-
-    var context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0, width, height);
-
-    return canvas;
-
-}
 function loadMolecule(url) {
 
-    for (var i = 0; i < objects.length; i++) {
+    while ( root.children.length > 0 ) {
 
-        var object = objects[i];
-        object.parent.remove(object);
+		var object = root.children[ 0 ];
+		object.parent.remove( object );
 
-    }
-
-    objects = [];
+	}
 
     loader.load(url, function (pdb) {
 
         var geometryAtoms = pdb.geometryAtoms;
         var geometryBonds = pdb.geometryBonds;
         var json = pdb.json;
+
+        var boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+        var sphereGeometry = new THREE.IcosahedronBufferGeometry(1, 2);
 
         geometryAtoms.computeBoundingBox();
         geometryAtoms.boundingBox.getCenter(offset).negate();
@@ -193,11 +106,43 @@ function loadMolecule(url) {
 
         var position = new THREE.Vector3();
         var color = new THREE.Color();
-        var start = new THREE.Vector3();
-        var end = new THREE.Vector3();
 
+        //plot atoms
+        positions = geometryAtoms.getAttribute('position');
+        for (var i = 0; i < positions.count; i++) {
+
+            position.x = positions.getX(i);
+            position.y = positions.getY(i);
+            position.z = positions.getZ(i);
+
+
+            color.r = colors.getX(i);
+            color.g = colors.getY(i);
+            color.b = colors.getZ(i);
+            
+            var material = new THREE.MeshPhongMaterial({ color: color });
+
+            var object = new THREE.Mesh(sphereGeometry, material);
+            object.position.copy(position);
+            object.position.multiplyScalar(75);
+            object.scale.multiplyScalar(25);
+            root.add(object);
+
+            var atom = json.atoms[i];
+
+            var text = document.createElement('div');
+            text.className = 'label';
+            text.style.color = 'rgb(' + atom[3][0] + ',' + atom[3][1] + ',' + atom[3][2] + ')';
+            text.textContent = atom[4];
+
+            var label = new CSS2DObject(text);
+            label.position.copy(object.position);
+            //root.add(label);
+        }
 
         //plot bond
+        var start = new THREE.Vector3();
+        var end = new THREE.Vector3();
         positions = geometryBonds.getAttribute('position');
         for (var i = 0; i < positions.count; i += 2) {
 
@@ -212,117 +157,12 @@ function loadMolecule(url) {
             start.multiplyScalar(75);
             end.multiplyScalar(75);
 
-            tmpVec1.subVectors(end, start);
-            //var bondLength = tmpVec1.length() - 50;
-            var bondLength = tmpVec1.length();
-
-            //
-
-            var bond = document.createElement('div');
-            bond.className = "bond";
-            bond.style.height = bondLength + "px";
-
-            var object = new CSS3DObject(bond);
+            var object = new THREE.Mesh(boxGeometry, new THREE.MeshPhongMaterial(0xffffff));
             object.position.copy(start);
             object.position.lerp(end, 0.5);
-
-            object.userData.bondLengthShort = bondLength + "px";
-            object.userData.bondLengthFull = (bondLength + 55) + "px";
-
-            //
-
-            var axis = tmpVec2.set(0, 1, 0).cross(tmpVec1);
-            var radians = Math.acos(tmpVec3.set(0, 1, 0).dot(tmpVec4.copy(tmpVec1).normalize()));
-
-            var objMatrix = new THREE.Matrix4().makeRotationAxis(axis.normalize(), radians);
-            object.matrix = objMatrix;
-            object.quaternion.setFromRotationMatrix(object.matrix);
-
-            object.matrixAutoUpdate = false;
-            object.updateMatrix();
-
+            object.scale.set(5, 5, start.distanceTo(end));
+            object.lookAt(end);
             root.add(object);
-
-            objects.push(object);
-
-            //
-
-            var bond = document.createElement('div');
-            bond.className = "bond";
-            bond.style.height = bondLength + "px";
-
-            var joint = new THREE.Object3D(bond);
-            joint.position.copy(start);
-            joint.position.lerp(end, 0.5);
-
-            joint.matrix.copy(objMatrix);
-            joint.quaternion.setFromRotationMatrix(joint.matrix);
-
-            joint.matrixAutoUpdate = false;
-            joint.updateMatrix();
-
-            var object = new CSS3DObject(bond);
-            object.rotation.y = Math.PI / 2;
-
-            object.matrixAutoUpdate = false;
-            object.updateMatrix();
-
-            object.userData.bondLengthShort = bondLength + "px";
-            object.userData.bondLengthFull = (bondLength + 55) + "px";
-
-            object.userData.joint = joint;
-
-            joint.add(object);
-            root.add(joint);
-
-            objects.push(object);
-
-        }
-        //plot atoms
-        positions = geometryAtoms.getAttribute('position');
-        for (var i = 0; i < positions.count; i++) {
-
-            position.x = positions.getX(i);
-            position.y = positions.getY(i);
-            position.z = positions.getZ(i);
-
-
-            color.r = colors.getX(i);
-            color.g = colors.getY(i);
-            color.b = colors.getZ(i);
-
-            var atom = json.atoms[i];
-            var element = atom[4];
-
-            if (!colorSpriteMap[element]) {
-
-                var canvas = imageToCanvas(baseSprite);
-                var context = canvas.getContext('2d');
-
-                colorify(context, canvas.width, canvas.height, color);
-
-                var dataUrl = canvas.toDataURL();
-
-                colorSpriteMap[element] = dataUrl;
-
-            }
-
-            var colorSprite = colorSpriteMap[element];
-
-            var atom = document.createElement('img');
-            atom.src = colorSprite;
-
-            var object = new CSS3DSprite(atom);
-            object.position.copy(position);
-            object.position.multiplyScalar(75);
-
-            object.matrixAutoUpdate = false;
-            object.updateMatrix();
-
-            root.add(object);
-
-            objects.push(object);
-
         }
         //plot box
         var box_positions = [
@@ -362,70 +202,12 @@ function loadMolecule(url) {
             start.multiplyScalar(75);
             end.multiplyScalar(75);
 
-            tmpVec1.subVectors(end, start);
-            var bondLength = tmpVec1.length()
-            //
-
-            var bond = document.createElement('div');
-            bond.className = "box-bond";
-            bond.style.height = bondLength + "px";
-
-            var object = new CSS3DObject(bond);
+            var object = new THREE.Mesh(boxGeometry, new THREE.MeshPhongMaterial(0xffffff));
             object.position.copy(start);
             object.position.lerp(end, 0.5);
-
-            object.userData.bondLengthShort = bondLength + "px";
-            object.userData.bondLengthFull = (bondLength + 55) + "px";
-
-            //
-
-            var axis = tmpVec2.set(0, 1, 0).cross(tmpVec1);
-            var radians = Math.acos(tmpVec3.set(0, 1, 0).dot(tmpVec4.copy(tmpVec1).normalize()));
-
-            var objMatrix = new THREE.Matrix4().makeRotationAxis(axis.normalize(), radians);
-            object.matrix = objMatrix;
-            object.quaternion.setFromRotationMatrix(object.matrix);
-
-            object.matrixAutoUpdate = false;
-            object.updateMatrix();
-
+            object.scale.set(5, 5, start.distanceTo(end));
+            object.lookAt(end);
             root.add(object);
-
-            objects.push(object);
-
-            //
-
-            var bond = document.createElement('div');
-            bond.className = "box-bond";
-            bond.style.height = bondLength + "px";
-
-            var joint = new THREE.Object3D(bond);
-            joint.position.copy(start);
-            joint.position.lerp(end, 0.5);
-
-            joint.matrix.copy(objMatrix);
-            joint.quaternion.setFromRotationMatrix(joint.matrix);
-
-            joint.matrixAutoUpdate = false;
-            joint.updateMatrix();
-
-            var object = new CSS3DObject(bond);
-            object.rotation.y = Math.PI / 2;
-
-            object.matrixAutoUpdate = false;
-            object.updateMatrix();
-
-            object.userData.bondLengthShort = bondLength + "px";
-            object.userData.bondLengthFull = (bondLength + 55) + "px";
-
-            object.userData.joint = joint;
-
-            joint.add(object);
-            root.add(joint);
-
-            objects.push(object);
-
-
         }
 
         switch (visualizationType) {
@@ -463,6 +245,7 @@ function animate() {
 function render() {
 
     renderer.render(scene, camera);
+    labelRenderer.render( scene, camera );
 
 }
 
@@ -477,6 +260,7 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(gmount.clientWidth, gmount.clientHeight);
+    //labelRenderer.setSize(gmount.clientWidth, gmount.clientHeight);
 }
 
 class VestaView extends Component {
