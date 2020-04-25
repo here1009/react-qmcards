07d878d02b4b6bbb10cdc1f36c6284747285d41d
here1009 <gao_yantao@yeah.net>
2020-04-25 16:37:38 +0800

    update task, give up develop on windows

---
+++ public/rustaddon/native/src/lib.rs
@@ -0,0 +1,10 @@
+extern crate num_cpus;
+use neon::prelude::*;
+
+
+fn thread_count(mut cx: FunctionContext) -> JsResult<JsNumber>{
+    Ok(cx.number(num_cpus::get() as f64))
+}
+register_module!(mut cx, {
+    cx.export_function("threadCount", thread_count)
+});
